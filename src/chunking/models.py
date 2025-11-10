b2cb51cd83e84cd59b5608e4d4696587dd82bf29
Petr Herčko <herckpet@cvut.cz>
2025-11-10 22:03:26 +0100

    main - added Chunk model, maybe better changing name to MilvusChunk

---
+++ src/chunking/models.py
@@ -0,0 +1,36 @@
+"""
+models
+"""
+import json
+import uuid
+
+from pydantic import BaseModel
+from typing import List, Optional, Any, Dict
+
+
+class Chunk(BaseModel):
+    id: uuid.UUID
+    doc_name: str
+    party: str
+    page_number: int
+    year: int
+    content: str
+    dense_vector: List[float]
+    metadata: Dict[str, Any]
+
+    async def to_milvus_dict(self) -> Dict:
+        """
+        Helper: Converts instance to dict for insertion into Milvus collection
+        """
+        milvus_dict = {
+            "id": str(self.id),
+            "doc_name": self.doc_name,
+            "party": self.party,
+            "page_number": self.page_number,
+            "year": self.year,
+            "content": self.content,
+            "dense_vector": self.dense_vector,
+            "metadata": json.dumps(self.metadata)  # must format to str for Milvus
+        }
+
+        return milvus_dict
